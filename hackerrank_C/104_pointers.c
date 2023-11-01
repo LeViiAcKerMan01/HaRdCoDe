fdca1e32b7efd586cc947e2b105764974dca0b12
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-11-01 15:35:15 +0530

    First commit

--- hackerrank_C/104_pointers.c
+++ hackerrank_C/104_pointers.c
@@ -1,15 +1,31 @@
 #include<stdio.h>
+#include<math.h>
 void update(int *a, int *b){
+    *a+=*b;
+    printf("%d\n", *a);
+
+    if(*b>*a){
+        *b-=*a;
+        printf("%d", *b);
+        
+    }
+    else{
+        int diff=*a-=*b;
+        printf("%d", diff);
+    }
+    }
 
 
-}
-int main() {
+int main()
+
+{
     int a, b;
-    int *pa = &a, *pb = &b;
-    
+    int *pa=&a;
+    int *pb=&b;
     scanf("%d %d", &a, &b);
-
-    printf("%d\n%d", a, b);
-
+    update(pa, pb);
+    
     return 0;
+    
+
 }
