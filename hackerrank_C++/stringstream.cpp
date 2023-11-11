bd24ad85855e83a7910168cd0ad7347341bf935a
Priyanshu Tadhiyal <124075086+LeViiAcKerMan01@users.noreply.github.com>
2023-11-11 10:11:35 +0530

    First commit

--- hackerrank_C++/stringstream.cpp
+++ hackerrank_C++/stringstream.cpp
@@ -8,12 +8,15 @@ int main() {
    string tmp; // A string to store the word on each iteration.
    stringstream str_strm(str);
    vector<string> words; // Create vector to hold our words
+   
    while (str_strm >> tmp) {
       // Provide proper checks here for tmp like if empty
       // Also strip down symbols like !, ., ?, etc.
       // Finally push it.
       words.push_back(tmp);
+      
    }
    for(int i = 0; i<words.size(); i++)
       cout << words[i] << endl;
+      
 }
